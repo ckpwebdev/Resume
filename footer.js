bc07130492b0b9eb6957e5f9f39c8032cd743885
ckpwebdev <ckp.webdev@gmail.com>
2026-02-17 12:05:39 -0700

    Updated footer.js

--- footer.js
+++ footer.js
@@ -9,18 +9,18 @@
       Built with
       <span class="tech-stack">
         HTML5
-        <img src="footer-images/sm-html.png" alt="HTML5" class="tech-icon">
+        <img src="footer-images/sm-html.png" alt="" aria-hidden="true" class="tech-icon" loading="lazy" width="16" height="16">
         , JavaScript
-        <img src="footer-images/sm-java.png" alt="JavaScript" class="tech-icon">
+        <img src="footer-images/sm-java.png" alt="" aria-hidden="true" class="tech-icon" loading="lazy" width="16" height="16">
         &amp; CSS3
-        <img src="footer-images/sm-css.png" alt="CSS3" class="tech-icon">
+        <img src="footer-images/sm-css.png" alt="" aria-hidden="true" class="tech-icon" loading="lazy" width="16" height="16">
       </span>
       using
       <span class="tech-tools">
         Visual Studio Code
-        <img src="footer-images/sm-vscode.png" alt="Visual Studio Code" class="tech-icon">
+        <img src="footer-images/sm-vscode.png" alt="" aria-hidden="true" class="tech-icon" loading="lazy" width="16" height="16">
         &amp; GitHub
-        <img src="footer-images/sm-github.png" alt="GitHub" class="tech-icon">
+        <img src="footer-images/sm-github.png" alt="" aria-hidden="true" class="tech-icon" loading="lazy" width="16" height="16">
       </span>
       .
     </p>
